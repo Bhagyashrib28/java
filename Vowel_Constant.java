db68775590d0d5edc9162e8518add57df22b2408
Bhagyashrib28 <bhagyashribelhekar28@gmail.com>
2026-01-26 23:10:26 +0530

    Create Vowel_Constant.java

---
+++ Vowel_Constant.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Vowel_Constant 
+{
+    
+    public static void main(String[] args) 
+    {
+
+
+        //vowel and constant
+        Scanner sc =new Scanner(System.in);
+         System.out.println("enter word : ");
+        String str1 = sc.next();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < str1.length(); i++) 
+        {
+            char ch = str1.charAt(i);
+
+            if (Character.isLetter(ch)) 
+            {
+                if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+                    ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') 
+                {
+                    vowels++;
+                } 
+                else
+                {
+                    consonants++;
+                }
+            }
+        }
+      
+        
+    }
+}
+
+    
+
