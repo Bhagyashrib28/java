29e5c3bb46334f9a0a0e5487faf67cb4d89ae542
Bhagyashrib28 <bhagyashribelhekar28@gmail.com>
2026-01-26 22:55:56 +0530

    Add string reversal and word count in Count.java
    Implement string reversal and word count functionality.

---
+++ Count.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class String1 
+{
+    public static void main(String[] args) 
+    {
+        //count string
+      
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("enter the string : ");
+        String str = sc.nextLine();
+        String count[]=str.split(" ");
+        System.out.println(Arrays.toString(count));
+        String reverse1 = "";
+
+        for (int i = 0; i < count.length; i++) 
+        {
+            String word = count[i];
+            String rev = "";
+
+            for (int j = word.length() - 1; j >= 0; j--) 
+            {
+                rev = rev + word.charAt(j);
+            }
+
+            reverse1 = reverse1 + rev + " ";
+        }
+
+        System.out.println("reverse string is : "+reverse1);
+        System.out.println("count of words is "+count.length);      
+
+    }
+
+
+    
+}
